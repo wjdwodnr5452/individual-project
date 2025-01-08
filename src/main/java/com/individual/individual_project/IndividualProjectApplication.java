e51b2bd0eb07d2c473b0e0cec4940ed4cf978ef9
wjdwodnr5452 <wjdwodnr5452@naver.com>
2025-01-08 21:27:49 +0900

    Initial commit

---
+++ src/main/java/com/individual/individual_project/IndividualProjectApplication.java
@@ -0,0 +1,13 @@
+package com.individual.individual_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualProjectApplication.class, args);
+	}
+
+}

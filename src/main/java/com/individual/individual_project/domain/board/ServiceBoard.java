58dee3d69b657e78c7ac56b0c0cf8759b018d53e
wjdwodnr5452 <wjdwodnr5452@naver.com>
2025-01-25 21:24:01 +0900

    게시글

--- src/main/java/com/individual/individual_project/domain/board/ServiceBoard.java
+++ src/main/java/com/individual/individual_project/domain/board/ServiceBoard.java
@@ -1,7 +1,10 @@
 package com.individual.individual_project.domain.board;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -12,12 +15,61 @@ public class ServiceBoard {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message ="BORD_NOT_TITLE")
     @Column(name = "service_title")
     private String serviceTitle;
 
-    @Column(name = "recruitCount")
-    private int recruitCount;
+    @NotNull(message ="BORD_NOT_RECRUIT_COUNT")
+    @Column(name = "recruit_count")
+    private Integer recruitCount;
 
+    @NotNull(message ="BORD_NOT_SERVICE_DATE")
+    @Column(name = "service_date")
+    private LocalDateTime serviceDate;
 
+    @NotNull(message ="BORD_NOT_SERVICE_TIME")
+    @Column(name = "service_time")
+    private Integer serviceTime;
 
+    @NotNull(message ="BORD_NOT_DEADLINE")
+    private LocalDateTime deadline;
+
+    @Column(name = "thumbnail_image")
+    private String thumbnailImage;
+
+    @NotNull(message ="BORD_NOT_USER_ID")
+    @Column(name = "user_id")
+    private Long userId;
+
+    @NotNull(message ="BORD_NOT_CATEGORY_ID")
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @NotNull(message ="BORD_NOT_SERVICE_STAT_ID")
+    @Column(name = "service_stat_id")
+    private Long serviceStatId;
+
+    @NotNull(message ="BORD_NOT_RECRUIT_STAT_ID")
+    @Column(name = "recruit_stat_id")
+    private Long recruitStatId;
+
+    @NotNull(message ="BORD_NOT_SERVICE_CONTENT")
+    @Column(name = "service_content")
+    private String serviceContent;
+    
+    public ServiceBoard() {}
+
+    public ServiceBoard(String serviceTitle, Integer recruitCount, LocalDateTime serviceDate, Integer serviceTime, LocalDateTime deadline, String thumbnailImage, Long userId, Long categoryId, Long serviceStatId, Long recruitStatId, String serviceContent) {
+        this.serviceTitle = serviceTitle;
+        this.recruitCount = recruitCount;
+        this.serviceDate = serviceDate;
+        this.serviceTime = serviceTime;
+        this.deadline = deadline;
+        this.thumbnailImage = thumbnailImage;
+        this.userId = userId;
+        this.categoryId = categoryId;
+        this.serviceStatId = serviceStatId;
+        this.recruitStatId = recruitStatId;
+        this.serviceContent = serviceContent;
+    }
 }
--- src/test/java/com/individual/individual_project/domain/board/BoardRepositoryTest.java
+++ src/test/java/com/individual/individual_project/domain/board/BoardRepositoryTest.java
@@ -5,18 +5,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
-@DataJpaTest
+@SpringBootTest
 @Transactional
 public class BoardRepositoryTest {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+
 
     @Test
     void findId() {
@@ -26,11 +29,8 @@ public class BoardRepositoryTest {
 
     @Test
     void getCategorys() {
-
         List<Category> all = categoryRepository.findAll();
-
         log.info(all.toString());
-
     }
 
 }
